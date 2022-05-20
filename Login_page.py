944ceb644829daa566db0def185438b342dea1a0
AThonnard <98116289+AThonnard@users.noreply.github.com>
2022-05-20 16:49:44 +0200

    Update Login_page.py

--- Login_page.py
+++ Login_page.py
@@ -6,22 +6,45 @@
 # If user logs in for the first time, he/she can create a login ==> another page#
 #################################################################################
 import webbrowser
+import mysql.connector
 from tkinter import *
 from tkinter.ttk import Combobox
 from tkinter import messagebox
 from PIL import ImageTk, Image
-#COMMAND CONNECT
-def entered_values():
+
+#CONNECT TO DATABASE FCSYNTRA
+fcsyntra_db = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "root",
+    database = "fcsyntra")
+
+mycursor = fcsyntra_db.cursor()
+
+#VERIFY LOGIN in DB
+def login_verify():
     username = username_entry.get()
     password = password_entry.get()
-
-    if(username == "" and password == ""):
-        messagebox.showinfo("", "Blank not allowed")
-
-    elif(username == "Admin" and password =="123"):
-        messagebox.showinfo("", "Login successful")
+    profile = profile_entry.get()
+    if username == "" or password == "" or profile =="":
+        messagebox.showinfo("", "Blanks not allowed")
     else:
-        messagebox.showinfo("", "Incorrect Username and Password")
+        if profile == 'Administrator':
+            sql = f"SELECT email from admin WHERE email = '{username}' AND paswoord = '{password}';"
+            mycursor.execute(sql)
+            if mycursor.fetchall():
+                messagebox.showinfo("", "Login Successful")
+                login_page.destroy()
+            else:
+                messagebox.showinfo("", "Incorrect Username and/or Password and/or profile")
+        else:
+            sql = f"SELECT email from leden WHERE email = '{username}' AND paswoord = '{password}';"
+            mycursor.execute(sql)
+            if mycursor.fetchall():
+                messagebox.showinfo("", "Login Successful")
+                login_page.destroy()
+            else:
+                messagebox.showinfo("", "Incorrect Username and/or Password and/or profile")
 
 def open_url_login_vergeten():
     webbrowser.open_new_tab('https://krcgenk.tickethour.be/l0322ww/accountPassword.html')
@@ -30,7 +53,6 @@ def open_creation_page():
     login_page.destroy()
     import Account_creation
 
-
 # creation page Main_Login_Page
 login_page = Tk()
 login_page.title("FC Syntra Genk")
@@ -38,10 +60,6 @@ login_page.state("zoomed")
 login_page.config(bg='DodgerBlue3')
 login_page.grid_rowconfigure(0, weight=1)
 login_page.grid_columnconfigure(0, weight=1)
-
-global username_entry
-global password_entry
-global Login_creation
 
 # TITLE PAGE
 L_Title = Label(login_page, text= "FC Syntra", fg='White', bg ='DodgerBlue3', font=('Helvetica',80))
@@ -85,11 +103,10 @@ Login_creation = Label(text="Nog geen account? Klik hier",bg="DodgerBlue2", fg="
 Login_creation.place(x = 400 , y = 700)
 Login_creation.bind("<Button-1>", lambda y:open_creation_page())
 
-
 ## TEKSTVAKKEN ##
 username = StringVar
 password = StringVar
-profile = StringVar
+
 #LOGIN
 username_entry = Entry(textvariable = username, width=50, highlightthickness=2)
 username_entry.place(x = 400 , y = 300, height = 30)
@@ -102,7 +119,9 @@ profile_entry = Combobox(login_page, values = values_profile, width=47)
 profile_entry.place(x = 400 , y = 500, height = 30)
 
 #BUTTON CONNECT
-login_button =Button(login_page, text="Connect", width="30", command= entered_values, bg ="White")
+login_button =Button(login_page, text="Connect", width="30", command= login_verify, bg ="White")
 login_button.place(x = 400 , y = 600, height = "30")
 
 login_page.mainloop()
+
+
